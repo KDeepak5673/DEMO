492a74734a78f3cbafdee8b24723b50bd24d4dea
KDeepak5673 <deepakchandrashekhar0309@gmail.com>
2025-09-22 14:48:05 +0530

    update readme

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite - React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -24,12 +24,13 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-      </div>
-      <p>This is a Vite + React template</p>
+        <p>This is a Vite + React template</p>
       <p > Demo for the Capestone project</p>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      </div>
+      
     </>
   )
 }
